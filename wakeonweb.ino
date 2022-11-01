cd98d86dc5cc45d011011c8475ef980e3d5cea21
jonasmusall <jonas.musall@gmail.com>
2022-11-01 10:20:50 +0100

    Move static strings to PROGMEM

--- wakeonweb.ino
+++ wakeonweb.ino
@@ -6,7 +6,7 @@
 #define REQUEST_RECORD_LEN 6
 
 ESP8266WebServer server(80);
-const String bodyRoot =
+const char bodyRoot[] PROGMEM =
     "<!DOCTYPE html><html><head>"
     "<meta charset=\"utf-8\">"
     "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
@@ -20,13 +20,13 @@ const String bodyRoot =
     "<p>SSH ready: <span class=\"state sshno\">NO</span><span class=\"state sshyes\">YES</span></p>"
     "<form action=\"/t\"><input class=\"pwroff\" type=\"submit\" value=\"Power on\"><input class=\"pwron\" type=\"submit\" value=\"Power on\" disabled></form>"
     "</body></html>";
-const String bodyMainStylesheet =
+const char bodyMainStylesheet[] PROGMEM =
     "html{min-height:100%;display:flex;justify-content:space-around;align-items:center;background:#0d0d0d;}"
     "body{height:fit-content;margin:0px;padding:1.2em 1.5em 1.5em 1.5em;font:1em sans-serif;background:white;border-radius:1em;}"
     "h1{margin-top:0px;}"
     ".state{padding:0.2em 0.8em;background:#6c6c6c;color:white;border-radius:1em;}"
     ".state.pwron,.state.sshyes{background:#008a3c;}.state.pwroff,.state.sshno{background:#b30000;}";
-const String bodyFavicon =
+const char bodyFavicon[] PROGMEM =
     "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" xmlns=\"http://www.w3.org/2000/svg\">"
     "<circle style=\"fill:#d7cb02;\" cx=\"12\" cy=\"12\" r=\"12\" />"
     "<path style=\"fill:#000000;\" d=\"M 16.763931,12 C 16.763662,10.895812 15.868608,10.000758 14.76442,10.000488 L 12,9.9999998 13,2 H 12 L 7.4487305,11.106445 C 7.3095717,11.383787 7.2368536,11.689704 7.2363281,12 c 2.694e-4,1.104188 0.8953234,1.999242 1.9995117,1.999512 L 12,14 11,22 h 1 l 4.551529,-9.106445 C 16.690688,12.616213 16.763406,12.310296 16.763931,12 Z\" />"
